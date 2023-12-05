757b3ee0878142312faa29e673eb262e86591879
Padma Patil <abdrazakovermek9@gmail.com>
2023-12-04 23:27:36 -0800

    Create an empty array and adds randomized number to it

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Pig-Dice
-Epicodus practice with js.
+```
+Descride: diceNumberRandomizer();
+
+Test: "It generates a random number between  1 and 6"
+Code: diceNumberRandomizer(6);
+Expected Output: true
+
+Descride: diceRollContainer
+
+Test: "It creates an empty array and adds  randomized number to it"
+Code: const result = diceRollContainer();
+console.log(result);
+Expected Output: true
+
+
+```
--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,28 @@
 function diceNumberRandomizer() {
     return Math.floor(Math.random() * 6);
 }
-console.log(diceNumberRandomizer(6));
+// console.log(diceNumberRandomizer(6));
 
+function diceRollContainer() {
+    const resultContainer = [];
 
+    const randomNumber = diceNumberRandomizer();
+    resultContainer.push(randomNumber);
+
+    return resultContainer;
+}
+// const result = diceRollContainer();
+// console.log(result);
+
+function totalScoreCounter(totalScore, currentScore) {
+    if (diceNumberRandomizer(1)) {
+        return totalScore; 0;
+    } else {
+        return currentScore + diceNumberRandomizer;
+    }
+}
+
+// console.log(totalScoreCounter(6, 5))
 
 
 // UI logic
